4ea0dc6ab4aaa629a3f5e4f45d3d8ce61f5cc42c
Ilya Starodubov <ilya.starodubov@fuse8.online>
2022-09-15 13:53:37 +0500

    Added client console logic

--- DepartmentApp/DepartmentApp/Program.cs
+++ DepartmentApp/DepartmentApp/Program.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using DepartmentApp.Services;
+using System;
 
 namespace DepartmentApp
 {
@@ -10,6 +7,49 @@ namespace DepartmentApp
     {
         static void Main(string[] args)
         {
+            DepartmentService departmentService = new DepartmentService();
+
+            string action = string.Empty;
+
+            while (action != "exit")
+            {
+                Console.WriteLine("Введите номер действия, которое хотите выполнить:");
+                Console.WriteLine("1. Получить суммарную зарплату в разрезе департаментов (без руководителей и с руководителями).");
+                Console.WriteLine("2. Получить департамент, в котором у сотрудника зарплата максимальна.");
+                Console.WriteLine("3. Получить зарплаты руководителей департаментов (по убыванию).");
+                Console.WriteLine("Для выхода из приложения введите Exit.");
+
+                action = Console.ReadLine().ToLower();
+
+                switch(action)
+                {
+                    case "1":
+                        Console.WriteLine("Включать зарплаты руководителей в расчет? (y/n)");
+                        string answer = Console.ReadLine().ToLower();
+
+                        while (answer != "y" && answer != "n")
+                        {
+                            Console.WriteLine("Введено некорректное значение. Введите Y или N.");
+                            answer = Console.ReadLine().ToLower();
+                        }
+
+                        Console.WriteLine(departmentService.GetSummarizedSalaryByDepartmentList(answer == "y"));
+                        break;
+                    case "2":
+                        Console.WriteLine(departmentService.GetDepartmentWithMaxSalary());
+                        break;
+                    case "3":
+                        Console.WriteLine(departmentService.GetChiefsSalariesDescList());
+                        break;
+                    case "exit":
+                        break;
+                    default:
+                        Console.WriteLine("Введено некорректное значение!");
+                        break;
+                }
+
+                Console.WriteLine();
+            }
         }
     }
 }
---
+++ DepartmentApp/DepartmentApp/Services/DepartmentService.cs
@@ -0,0 +1,44 @@
+﻿namespace DepartmentApp.Services
+{
+    /// <summary>
+    /// Сервис для работы с департаментами
+    /// </summary>
+    public class DepartmentService
+    {
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        public DepartmentService()
+        {
+
+        }
+
+        /// <summary>
+        /// Получить суммарную зарплату в разрезе департаментов (без руководителей и с руководителями)
+        /// </summary>
+        /// <param name="includeChief">Флаг включения/исключения руководителей департаментов в подсчет суммарной зарплаты</param>
+        /// <returns></returns>
+        public string GetSummarizedSalaryByDepartmentList(bool includeChief = false)
+        {
+            return null;//List<DepartmentSalaryAttributes>
+        }
+
+        /// <summary>
+        /// Получить департамент, в котором у сотрудника зарплата максимальна
+        /// </summary>
+        /// <returns></returns>
+        public string GetDepartmentWithMaxSalary()
+        {
+            return null;//DepartmentSalaryAttributes
+        }
+
+        /// <summary>
+        /// Получить зарплаты руководителей департаментов (по убыванию)
+        /// </summary>
+        /// <returns></returns>
+        public string GetChiefsSalariesDescList()
+        {
+            return null;//List<ChiefDepartmentSalaryAttributes>
+        }
+    }
+}
