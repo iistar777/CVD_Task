47a4663eb884ab42217d51082be8d30f4a14fa89
Ilya Starodubov <ilya.starodubov@fuse8.online>
2022-09-15 11:51:46 +0500

    Added entities

---
+++ DepartmentApp/DepartmentApp/Models/Department.cs
@@ -0,0 +1,18 @@
+﻿namespace DepartmentApp.Models
+{
+    /// <summary>
+    /// Департамент
+    /// </summary>
+    public class Department
+    {
+        /// <summary>
+        /// Идентификатор департамента
+        /// </summary>
+        public int ID { get; set; }
+
+        /// <summary>
+        /// Наименование департамента
+        /// </summary>
+        public string Name { get; set; }
+    }
+}
